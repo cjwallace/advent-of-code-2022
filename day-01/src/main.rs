1b02035c8d3954c95bb84f71e4b404cce8f46f01
Chris Wallace <chris@bitcollider.io>
2022-12-01 12:03:25 +0000

    Solve day 01, part 02

--- day-01/src/main.rs
+++ day-01/src/main.rs
@@ -1,21 +1,34 @@
+use itertools::Itertools;
 use std::fs;
 
 fn read_data(path: &str) -> String {
     fs::read_to_string(path).expect("That's not a valid input file")
 }
 
-fn part1(data: String) -> i32 {
+fn data_to_calories(data: String) -> Vec<u32> {
     data.split("\n\n")
-        .map(|elf| elf.split("\n").map(|food| food.parse::<i32>().unwrap()))
-        .map(|elf| elf.sum::<i32>())
-        .max()
-        .unwrap()
+        .map(|elf| {
+            elf.split("\n")
+                .map(|calories| calories.parse::<u32>().unwrap())
+                .sum()
+        })
+        .collect()
+}
+
+fn part1(calories: &[u32]) -> u32 {
+    *calories.iter().max().unwrap()
+}
+
+fn part2(calories: &[u32]) -> u32 {
+    calories.iter().sorted().rev().take(3).sum()
 }
 
 fn main() {
-    let data = read_data("../data/input.txt");
+    let data = read_data("day-01/data/input.txt");
+    let calories = data_to_calories(data);
 
-    println!("Part 1: {}", part1(data));
+    println!("Part 1: {}", part1(&calories));
+    println!("Part 2: {}", part2(&calories));
 }
 
 #[cfg(test)]
@@ -25,6 +38,14 @@ mod test {
     #[test]
     fn test_part1() {
         let data = read_data("data/test.txt");
-        assert_eq!(part1(data), 24000)
+        let calories = data_to_calories(data);
+        assert_eq!(part1(&calories), 24000)
+    }
+
+    #[test]
+    fn test_part2() {
+        let data = read_data("data/test.txt");
+        let calories = data_to_calories(data);
+        assert_eq!(part2(&calories), 45000)
     }
 }
