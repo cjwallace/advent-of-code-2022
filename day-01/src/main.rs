98d60eb4f40eb28ab152108b69b5e929377ec86d
Chris Wallace <chris@bitcollider.io>
2022-12-01 17:53:12 +0000

    Tidy day 01
    Reconcile solutions to part one and part two into a single function.

--- day-01/src/main.rs
+++ day-01/src/main.rs
@@ -1,34 +1,26 @@
 use itertools::Itertools;
 use std::fs;
 
-fn read_data(path: &str) -> String {
-    fs::read_to_string(path).expect("That's not a valid input file")
-}
-
-fn data_to_calories(data: String) -> Vec<u32> {
-    data.split("\n\n")
+fn calories(path: &str, n: usize) -> u32 {
+    fs::read_to_string(path)
+        .expect("That's not a valid input file")
+        .split("\n\n")
         .map(|elf| {
-            elf.split("\n")
+            elf.split('\n')
                 .map(|calories| calories.parse::<u32>().unwrap())
-                .sum()
+                .sum::<u32>()
         })
-        .collect()
-}
-
-fn part1(calories: &[u32]) -> u32 {
-    *calories.iter().max().unwrap()
-}
-
-fn part2(calories: &[u32]) -> u32 {
-    calories.iter().sorted().rev().take(3).sum()
+        .sorted()
+        .rev()
+        .take(n)
+        .sum()
 }
 
 fn main() {
-    let data = read_data("day-01/data/input.txt");
-    let calories = data_to_calories(data);
+    let input = "day-01/data/input.txt";
 
-    println!("Part 1: {}", part1(&calories));
-    println!("Part 2: {}", part2(&calories));
+    println!("Part one: {}", calories(&input, 1));
+    println!("Part two: {}", calories(&input, 3));
 }
 
 #[cfg(test)]
@@ -36,16 +28,14 @@ mod test {
     use super::*;
 
     #[test]
-    fn test_part1() {
-        let data = read_data("data/test.txt");
-        let calories = data_to_calories(data);
-        assert_eq!(part1(&calories), 24000)
+    fn test_part_one() {
+        let test = "data/test.txt";
+        assert_eq!(calories(test, 1), 24000);
     }
 
     #[test]
-    fn test_part2() {
-        let data = read_data("data/test.txt");
-        let calories = data_to_calories(data);
-        assert_eq!(part2(&calories), 45000)
+    fn test_part_two() {
+        let test = "data/test.txt";
+        assert_eq!(calories(test, 3), 45000)
     }
 }
