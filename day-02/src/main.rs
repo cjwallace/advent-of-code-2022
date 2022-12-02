1e746257e07c12c0bc1070fa6e36e479bc3c9e20
Chris Wallace <chris@bitcollider.io>
2022-12-02 09:42:16 +0000

    Solve day 02, part 02

--- day-02/src/main.rs
+++ day-02/src/main.rs
@@ -19,9 +19,29 @@ fn part_one(path: &str) -> u32 {
         .sum()
 }
 
+fn part_two(path: &str) -> u32 {
+    fs::read_to_string(path)
+        .expect("That is not a valid input")
+        .split('\n')
+        .map(|game| match game {
+            "A X" => 3 + 0, // choice + result
+            "A Y" => 1 + 3,
+            "A Z" => 2 + 6,
+            "B X" => 1 + 0,
+            "B Y" => 2 + 3,
+            "B Z" => 3 + 6,
+            "C X" => 2 + 0,
+            "C Y" => 3 + 3,
+            "C Z" => 1 + 6,
+            _ => 0,
+        })
+        .sum()
+}
+
 fn main() {
     let path = "day-02/data/input.txt";
     println!("Part one: {}", part_one(&path));
+    println!("Part two: {}", part_two(&path));
 }
 
 #[cfg(test)]
@@ -32,5 +52,11 @@ mod tests {
     fn test_part_one() {
         let path = "data/test.txt";
         assert_eq!(part_one(path), 15);
+    }
+
+    #[test]
+    fn test_part_two() {
+        let path = "data/test.txt";
+        assert_eq!(part_two(path), 12);
     }
 }
