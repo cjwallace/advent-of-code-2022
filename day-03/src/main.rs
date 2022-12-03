8276b5ce44a95666d61991ecf999a198d825c0c5
Chris Wallace <chris@bitcollider.io>
2022-12-03 12:12:41 +0000

    Tidy up day 03

--- day-03/src/main.rs
+++ day-03/src/main.rs
@@ -1,55 +1,70 @@
-use itertools::Itertools;
 use std::collections::HashSet;
 use std::fs;
 
-fn part_one(path: &str) -> u32 {
+type Pack = (HashSet<char>, HashSet<char>);
+
+fn packs(path: &str) -> Vec<Pack> {
     fs::read_to_string(path)
         .expect("That's not a valid input file")
         .split('\n')
         .map(|pack| {
-            let mid = (pack.len() / 2) as usize;
-            let (first, last) = pack.split_at(mid);
-            let a = HashSet::<char>::from_iter(first.chars());
-            let b = HashSet::<char>::from_iter(last.chars());
-            let c = a.intersection(&b).next().unwrap().to_owned();
-            if c.is_lowercase() {
-                c as u32 - 'a' as u32 + 1
-            } else {
-                c as u32 - 'A' as u32 + 27
-            }
+            let compartments = pack.split_at(pack.len() / 2);
+            (
+                compartments.0.chars().collect(),
+                compartments.1.chars().collect(),
+            )
+        })
+        .collect::<Vec<Pack>>()
+}
+
+fn points(c: char) -> u32 {
+    if c.is_lowercase() {
+        c as u32 - 'a' as u32 + 1
+    } else {
+        c as u32 - 'A' as u32 + 27
+    }
+}
+
+fn part_one(packs: &[Pack]) -> u32 {
+    packs
+        .iter()
+        .map(|pack| {
+            let (one, two) = pack;
+            let item = one.intersection(two).next().unwrap().to_owned();
+            points(item)
         })
         .sum()
 }
 
-fn part_two(path: &str) -> u32 {
-    fs::read_to_string(path)
-        .expect("That's not a valid input file")
-        .split('\n')
+fn part_two(packs: &[Pack]) -> u32 {
+    packs
         .chunks(3)
-        .into_iter()
-        .map(|chunk| {
-            let elves = chunk.map(|elf| HashSet::<char>::from_iter(elf.chars()));
+        .map(|elves| {
             let badge = elves
-                .reduce(|common_items, elf| common_items.intersection(&elf).cloned().collect())
+                .iter()
+                // combine compartments
+                .map(|pack| pack.0.union(&pack.1).cloned().collect())
+                // find common item
+                .reduce(|common_items: HashSet<char>, pack: HashSet<char>| {
+                    common_items.intersection(&pack).cloned().collect()
+                })
+                // rust is silly
                 .unwrap()
                 .iter()
                 .next()
                 .unwrap()
                 .to_owned();
 
-            if badge.is_lowercase() {
-                badge as u32 - 'a' as u32 + 1
-            } else {
-                badge as u32 - 'A' as u32 + 27
-            }
+            points(badge)
         })
         .sum()
 }
 
 fn main() {
     let path = "day-03/data/input.txt";
-    println!("Part one: {}", part_one(path));
-    println!("Part one: {}", part_two(path));
+    let packs = packs(path);
+    println!("Part one: {}", part_one(&packs));
+    println!("Part one: {}", part_two(&packs));
 }
 
 #[cfg(test)]
@@ -59,12 +74,14 @@ mod test {
     #[test]
     fn test_part_one() {
         let path = "data/test.txt";
-        assert_eq!(part_one(path), 157);
+        let packs = packs(path);
+        assert_eq!(part_one(&packs), 157);
     }
 
     #[test]
     fn test_part_two() {
         let path = "data/test.txt";
-        assert_eq!(part_two(path), 70);
+        let packs = packs(path);
+        assert_eq!(part_two(&packs), 70);
     }
 }
