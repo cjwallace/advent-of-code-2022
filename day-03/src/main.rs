8ebc64d2237b9cbd1aa57bc056c14c20124a5744
Chris Wallace <chris@bitcollider.io>
2022-12-03 11:16:25 +0000

    Solve day 03, part 02

--- day-03/src/main.rs
+++ day-03/src/main.rs
@@ -1,3 +1,4 @@
+use itertools::Itertools;
 use std::collections::HashSet;
 use std::fs;
 
@@ -20,9 +21,35 @@ fn part_one(path: &str) -> u32 {
         .sum()
 }
 
+fn part_two(path: &str) -> u32 {
+    fs::read_to_string(path)
+        .expect("That's not a valid input file")
+        .split('\n')
+        .chunks(3)
+        .into_iter()
+        .map(|chunk| {
+            let elves = chunk.map(|elf| HashSet::<char>::from_iter(elf.chars()));
+            let badge = elves
+                .reduce(|common_items, elf| common_items.intersection(&elf).cloned().collect())
+                .unwrap()
+                .iter()
+                .next()
+                .unwrap()
+                .to_owned();
+
+            if badge.is_lowercase() {
+                badge as u32 - 'a' as u32 + 1
+            } else {
+                badge as u32 - 'A' as u32 + 27
+            }
+        })
+        .sum()
+}
+
 fn main() {
     let path = "day-03/data/input.txt";
     println!("Part one: {}", part_one(path));
+    println!("Part one: {}", part_two(path));
 }
 
 #[cfg(test)]
@@ -33,5 +60,11 @@ mod test {
     fn test_part_one() {
         let path = "data/test.txt";
         assert_eq!(part_one(path), 157);
+    }
+
+    #[test]
+    fn test_part_two() {
+        let path = "data/test.txt";
+        assert_eq!(part_two(path), 70);
     }
 }
