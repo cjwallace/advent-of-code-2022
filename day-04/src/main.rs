62ea2d7f910b540330ae21a8df48a4d13fee2c10
Chris Wallace <chris@bitcollider.io>
2022-12-04 10:52:46 +0000

    Solve day 04, part 02

--- day-04/src/main.rs
+++ day-04/src/main.rs
@@ -1,25 +1,35 @@
 use std::fs;
 
-fn part_one(path: &str) -> u32 {
+fn condition_one(fl: u32, fu: u32, sl: u32, su: u32) -> bool {
+    ((fl <= sl) & (fu >= su)) | ((fl >= sl) & (fu <= su))
+}
+
+fn condition_two(fl: u32, fu: u32, sl: u32, su: u32) -> bool {
+    ((fu >= sl) & (su >= fl)) | ((su >= fl) & (fu >= sl))
+}
+
+fn parse_sections(elf: &str) -> (u32, u32) {
+    let (lower, upper) = elf.split_once('-').unwrap();
+    (lower.parse::<u32>().unwrap(), upper.parse::<u32>().unwrap())
+}
+
+fn solution(path: &str, condition: fn(u32, u32, u32, u32) -> bool) -> usize {
     fs::read_to_string(path)
         .expect("That is not a valid input file")
         .split('\n')
         .filter(|line| {
             let (first_elf, second_elf) = line.split_once(",").unwrap();
-            let (first_lower, first_upper) = first_elf.split_once('-').unwrap();
-            let (second_lower, second_upper) = second_elf.split_once('-').unwrap();
-            let fl = first_lower.parse::<u32>().unwrap();
-            let fu = first_upper.parse::<u32>().unwrap();
-            let sl = second_lower.parse::<u32>().unwrap();
-            let su = second_upper.parse::<u32>().unwrap();
-            ((fl <= sl) & (fu >= su)) | ((fl >= sl) & (fu <= su))
+            let (first_lower, first_upper) = parse_sections(first_elf);
+            let (second_lower, second_upper) = parse_sections(second_elf);
+            condition(first_lower, first_upper, second_lower, second_upper)
         })
-        .count() as u32
+        .count()
 }
 
 fn main() {
     let path = "day-04/data/input.txt";
-    println!("Part one: {}", part_one(path))
+    println!("Part one: {}", solution(path, condition_one));
+    println!("Part two: {}", solution(path, condition_two));
 }
 
 #[cfg(test)]
@@ -29,6 +39,12 @@ mod test {
     #[test]
     fn test_part_one() {
         let path = "data/test.txt";
-        assert_eq!(part_one(path), 2);
+        assert_eq!(solution(path, condition_one), 2);
+    }
+
+    #[test]
+    fn test_part_two() {
+        let path = "data/test.txt";
+        assert_eq!(solution(path, condition_two), 4);
     }
 }
