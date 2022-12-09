2ccf7d8f2b617da4928679fb073235678cd0b408
Chris Wallace <chris@bitcollider.io>
2022-12-09 21:17:31 +0000

    Refactor day 09 to custom types and traits
    I'm not sure this is any clearer than the original bag of functions, to be honest.

--- day-09/src/main.rs
+++ day-09/src/main.rs
@@ -3,72 +3,89 @@ use std::fs::read_to_string;
 use std::num::ParseIntError;
 use std::str::FromStr;
 
-type Coord = (i32, i32);
-type Rope = Vec<Coord>;
-
-struct Move {
-    direction: Coord,
-    steps: usize,
-}
-
 const L: Coord = (-1, 0);
 const R: Coord = (1, 0);
 const U: Coord = (0, 1);
 const D: Coord = (0, -1);
 
+fn main() {
+    let path = "day-09/data/input.txt";
+    println!("Part one: {}", positions_visited(path, 2));
+    println!("Part two: {}", positions_visited(path, 10));
+}
+
 fn positions_visited(path: &str, length_of_rope: usize) -> usize {
     let input = read_to_string(path).expect("That is not a valid input");
 
-    let mut rope_coords = vec![vec![(0, 0); length_of_rope]];
+    let mut rope = RopePositions {
+        current: vec![(0, 0); length_of_rope],
+        history: vec![],
+    };
 
     for line in input.lines() {
         let m = Move::from_str(line).unwrap();
-        rope_coords = update_coords(rope_coords, m);
+        rope = rope.update(m);
     }
 
-    count_tail_positions(rope_coords)
+    rope.count_tail_positions()
 }
 
-fn update_coords(mut ropes: Vec<Rope>, m: Move) -> Vec<Rope> {
-    for _ in 0..m.steps {
-        let prev = ropes.last().unwrap();
-        let new_head = move_knot(prev[0], m.direction);
-        let new_rope = new_rope(&prev, new_head);
-        ropes.push(new_rope)
+type Coord = (i32, i32);
+type Rope = Vec<Coord>;
+
+struct RopePositions {
+    current: Rope,
+    history: Vec<Rope>,
+}
+
+impl RopePositions {
+    fn update(mut self, m: Move) -> Self {
+        for _ in 0..m.steps {
+            let new_head = self.move_knot(&self.current[0], &m.direction);
+            let new_rope = self.new_rope(&self.current, new_head);
+            self.history.push(self.current);
+            self.current = new_rope;
+        }
+        self
     }
-    ropes
-}
 
-fn new_rope(previous_rope: &Rope, new_head: Coord) -> Rope {
-    previous_rope
-        .iter()
-        .skip(1)
-        .fold(vec![new_head], |mut acc, knot| {
-            let new_knot = move_tail(&knot, &acc.last().unwrap());
-            acc.push(new_knot);
-            acc
-        })
-}
+    fn count_tail_positions(&self) -> usize {
+        vec![self.current.clone()]
+            .iter()
+            .chain(self.history.iter())
+            .map(|coord| coord.last().unwrap())
+            .unique()
+            .count()
+    }
 
-fn move_knot(position: Coord, change: Coord) -> Coord {
-    (position.0 + change.0, position.1 + change.1)
-}
+    fn new_rope(&self, previous_rope: &Rope, new_head: Coord) -> Rope {
+        previous_rope
+            .iter()
+            .skip(1)
+            .fold(vec![new_head], |mut acc, knot| {
+                let new_knot = self.move_tail(&knot, &acc.last().unwrap());
+                acc.push(new_knot);
+                acc
+            })
+    }
 
-fn move_tail(tail: &Coord, head: &Coord) -> Coord {
-    let (x, y) = (head.0 - tail.0, head.1 - tail.1);
-    if x.abs() <= 1 && y.abs() <= 1 {
-        *tail
-    } else {
-        move_knot(*tail, (x.signum(), y.signum()))
+    fn move_tail(&self, tail: &Coord, head: &Coord) -> Coord {
+        let (x, y) = (head.0 - tail.0, head.1 - tail.1);
+        if x.abs() <= 1 && y.abs() <= 1 {
+            *tail
+        } else {
+            self.move_knot(tail, &(x.signum(), y.signum()))
+        }
+    }
+
+    fn move_knot(&self, position: &Coord, change: &Coord) -> Coord {
+        (position.0 + change.0, position.1 + change.1).clone()
     }
 }
 
-fn count_tail_positions(ropes: Vec<Rope>) -> usize {
-    ropes
-        .iter()
-        .map(|coord| coord.last().clone())
-        .unique()
-        .count()
+struct Move {
+    direction: Coord,
+    steps: usize,
 }
 
 impl FromStr for Move {
@@ -89,12 +106,6 @@ impl FromStr for Move {
 
         Ok(Move { direction, steps })
     }
-}
-
-fn main() {
-    let path = "day-09/data/input.txt";
-    println!("Part one: {}", positions_visited(path, 2));
-    println!("Part two: {}", positions_visited(path, 10));
 }
 
 #[cfg(test)]
