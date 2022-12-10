b93e5cf57d0b6676ff45045bb81d004e013bb716
Chris Wallace <chris@bitcollider.io>
2022-12-10 15:59:22 +0000

    Golf day 10 down a bit

--- day-10/src/main.rs
+++ day-10/src/main.rs
@@ -1,43 +1,42 @@
-use std::str::FromStr;
-use std::{error, fs};
+use std::fs;
 
 fn main() {
     let path = "day-10/data/input.txt";
-    println!("Part one: {}", part_one(path));
-    println!("Part two:");
-    part_two(path);
-}
-
-fn part_one(path: &str) -> i32 {
     let input = fs::read_to_string(path).expect("That is not a valid input");
     let cycles = clock_circuit(&input);
+    println!("Part one: {}", part_one(&cycles));
+    println!("Part two:");
+    part_two(&cycles);
+}
 
-    let signal_strength = 20 * cycles[19]
+fn part_one(cycles: &Vec<i32>) -> i32 {
+    20 * cycles[19]
         + 60 * cycles[59]
         + 100 * cycles[99]
         + 140 * cycles[139]
         + 180 * cycles[179]
-        + 220 * cycles[219];
-    signal_strength
+        + 220 * cycles[219]
 }
 
-fn part_two(path: &str) {
-    let input = fs::read_to_string(path).expect("That is not a valid input");
-    let cycles = clock_circuit(&input);
-
-    let screen: Vec<&str> = cycles
+fn part_two(cycles: &Vec<i32>) {
+    let screen: Vec<char> = cycles
         .iter()
         .enumerate()
         .map(|(i, x)| {
             if (i.rem_euclid(40) as i32 - x).abs() <= 1 {
-                "#"
+                '#'
             } else {
-                "."
+                '.'
             }
         })
         .collect();
 
-    print_crt(screen);
+    let pixels: String = screen
+        .chunks(40)
+        .map(|row| String::from_iter(row.iter()))
+        .collect::<Vec<String>>()
+        .join("\n");
+    println!("{}", pixels);
 }
 
 fn clock_circuit(input: &str) -> Vec<i32> {
@@ -45,52 +44,17 @@ fn clock_circuit(input: &str) -> Vec<i32> {
     let mut cycles = Vec::<i32>::new();
 
     for line in input.lines() {
-        let instruction = Instruction::from_str(line).unwrap();
-        match instruction {
-            Instruction::NoOp => {
-                cycles.push(register);
-            }
-            Instruction::AddX(x) => {
-                cycles.push(register);
-                cycles.push(register);
-                register += x;
-            }
+        if line.starts_with("addx") {
+            let (_, n) = line.split_once(' ').unwrap();
+            cycles.push(register);
+            cycles.push(register);
+            register += n.parse::<i32>().unwrap();
+        } else {
+            cycles.push(register);
         }
     }
 
     cycles
-}
-
-fn print_crt(v: Vec<&str>) {
-    let line_length = 40;
-    for i in 0..6 {
-        print_crt_line(&v[i * line_length..(i + 1) * line_length]);
-    }
-}
-
-fn print_crt_line(output: &[&str]) {
-    for s in output {
-        print!("{}", s);
-    }
-    println!();
-}
-
-enum Instruction {
-    AddX(i32),
-    NoOp,
-}
-
-impl FromStr for Instruction {
-    type Err = Box<dyn error::Error>;
-
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        if s.starts_with("addx") {
-            let (_, n) = s.split_once(' ').unwrap();
-            Ok(Instruction::AddX(n.parse::<i32>().unwrap()))
-        } else {
-            Ok(Instruction::NoOp)
-        }
-    }
 }
 
 #[cfg(test)]
@@ -100,12 +64,16 @@ mod test {
     #[test]
     fn test_part_one() {
         let path = "data/test.txt";
-        assert_eq!(part_one(path), 13140);
+        let input = fs::read_to_string(path).expect("That is not a valid input");
+        let cycles = clock_circuit(&input);
+        assert_eq!(part_one(&cycles), 13140);
     }
 
     #[test]
     fn test_part_two() {
         let path = "data/test.txt";
-        part_two(path);
+        let input = fs::read_to_string(path).expect("That is not a valid input");
+        let cycles = clock_circuit(&input);
+        part_two(&cycles);
     }
 }
